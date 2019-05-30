5cf380de0c5520292cd5c031612d0a5072925117
EricPKerr <EricPKerr@gmail.com>
2019-05-30 12:01:11 -0500

    Update eslint

--- src/server.js
+++ src/server.js
@@ -11,13 +11,15 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware({
-			session: (req, res) => ({ config })
-		})
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err)
-	});
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware({
+      session: (req, res) => ({
+        config
+      })
+    })
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
