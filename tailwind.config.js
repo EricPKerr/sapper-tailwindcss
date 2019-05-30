f1101f5e1bd7a3ff49aa5888f8a982f532d14745
EricPKerr <EricPKerr@gmail.com>
2019-05-30 12:27:00 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
 const colors = {
-	"dbnomics-blue-darkest": "#0e2730",
-	"dbnomics-blue-darker": "#12323d",
-	"dbnomics-green-dark": "#4f7061",
-	"dbnomics-green": "#608e75",
-	"dbnomics-green-lighter": "#b0d9bf"
-}
+  'primary-blue': '#0e2730'
+};
 
 module.exports = {
-	theme: {
-		container: {
-			center: true,
-			padding: "2rem"
-		},
-		extend: {
-			colors
-		}
-	},
-	variants: {},
-	plugins: []
-}
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {
+      colors
+    }
+  },
+  variants: {},
+  plugins: []
+};
